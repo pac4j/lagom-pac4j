51da323529987addbec69e4b5d62442d112bc83b
Sergey Morgunov <hostage@me.com>
2020-11-03 13:46:29 +0300

    Support initialize `JwtAuthenticator` by urls of JWKS (#34)

--- shared/src/main/java/org/pac4j/lagom/jwt/JwtAuthenticatorHelper.java
+++ shared/src/main/java/org/pac4j/lagom/jwt/JwtAuthenticatorHelper.java
@@ -6,8 +6,15 @@ import com.nimbusds.jose.JWEAlgorithm;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKMatcher;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
 import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigRenderOptions;
 import org.pac4j.jwt.config.encryption.AbstractEncryptionConfiguration;
@@ -22,9 +29,15 @@ import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
 import org.pac4j.jwt.config.signature.SignatureConfiguration;
 import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.nimbusds.jose.jwk.source.RemoteJWKSet.DEFAULT_HTTP_CONNECT_TIMEOUT;
+import static com.nimbusds.jose.jwk.source.RemoteJWKSet.DEFAULT_HTTP_READ_TIMEOUT;
+import static com.nimbusds.jose.jwk.source.RemoteJWKSet.DEFAULT_HTTP_SIZE_LIMIT;
 
 /**
  * Helper for parsing {@link JwtAuthenticator} from Lagom configuration.
@@ -34,10 +47,8 @@ import java.util.List;
  */
 public final class JwtAuthenticatorHelper {
 
-    private static SignatureConfiguration parseSignature(Config conf) throws ParseException, JOSEException {
-        if (!conf.hasPath("jwk")) return null;
+    private static AbstractSignatureConfiguration parseSignature(JWK jwk) throws JOSEException {
         AbstractSignatureConfiguration signature = null;
-        JWK jwk = JWK.parse(conf.getConfig("jwk").root().render(ConfigRenderOptions.concise()));
         if (jwk instanceof OctetSequenceKey) {
             signature = new SecretSignatureConfiguration(((OctetSequenceKey) jwk).toByteArray());
         } else if (jwk instanceof RSAKey) {
@@ -45,16 +56,22 @@ public final class JwtAuthenticatorHelper {
         } else if (jwk instanceof ECKey) {
             signature = new ECSignatureConfiguration(((ECKey) jwk).toKeyPair());
         }
+        return signature;
+    }
+
+    private static SignatureConfiguration parseSignature(Config conf) throws ParseException, JOSEException {
+        if (!conf.hasPath("jwk")) return null;
+        AbstractSignatureConfiguration signature = parseSignature(
+            JWK.parse(conf.getConfig("jwk").root().render(ConfigRenderOptions.concise()))
+        );
         if (signature != null && conf.hasPath("algorithm")) {
             signature.setAlgorithm(JWSAlgorithm.parse(conf.getString("algorithm")));
         }
         return signature;
     }
 
-    private static EncryptionConfiguration parseEncryption(Config conf) throws ParseException, JOSEException {
-        if (!conf.hasPath("jwk")) return null;
+    private static AbstractEncryptionConfiguration parseEncryption(JWK jwk) throws JOSEException {
         AbstractEncryptionConfiguration encryption = null;
-        JWK jwk = JWK.parse(conf.getConfig("jwk").root().render(ConfigRenderOptions.concise()));
         if (jwk instanceof OctetSequenceKey) {
             encryption = new SecretEncryptionConfiguration(((OctetSequenceKey) jwk).toByteArray());
         } else if (jwk instanceof RSAKey) {
@@ -62,6 +79,14 @@ public final class JwtAuthenticatorHelper {
         } else if (jwk instanceof ECKey) {
             encryption = new ECEncryptionConfiguration(((ECKey) jwk).toKeyPair());
         }
+        return encryption;
+    }
+
+    private static EncryptionConfiguration parseEncryption(Config conf) throws ParseException, JOSEException {
+        if (!conf.hasPath("jwk")) return null;
+        AbstractEncryptionConfiguration encryption = parseEncryption(
+            JWK.parse(conf.getConfig("jwk").root().render(ConfigRenderOptions.concise()))
+        );
         if (encryption != null) {
             if (conf.hasPath("algorithm")) encryption.setAlgorithm(JWEAlgorithm.parse(conf.getString("algorithm")));
             if (conf.hasPath("method")) encryption.setMethod(EncryptionMethod.parse(conf.getString("method")));
@@ -77,9 +102,35 @@ public final class JwtAuthenticatorHelper {
      * @throws ParseException a parse exception
      * @throws JOSEException a signing/encryption exception
      */
-    public static JwtAuthenticator parse(Config conf) throws ParseException, JOSEException {
+    public static JwtAuthenticator parse(Config conf) throws ParseException, JOSEException, MalformedURLException {
         List<SignatureConfiguration> signatures = new ArrayList<>();
         List<EncryptionConfiguration> encryptions = new ArrayList<>();
+        ResourceRetriever jwkRetriever = null;
+        if (conf.hasPath("jwk-retriever")) {
+            Config retrieverConf = conf.getConfig("jwk-retriever");
+            jwkRetriever = new DefaultResourceRetriever(
+                retrieverConf.hasPath("connect-timeout") ? retrieverConf.getInt("connect-timeout") : DEFAULT_HTTP_CONNECT_TIMEOUT,
+                retrieverConf.hasPath("read-timeout") ? retrieverConf.getInt("read-timeout") : DEFAULT_HTTP_READ_TIMEOUT,
+                retrieverConf.hasPath("size-limit") ? retrieverConf.getInt("size-limit") : DEFAULT_HTTP_SIZE_LIMIT
+            );
+        }
+        if (conf.hasPath("jwk-urls")) {
+            for (String jwkUrl : conf.getStringList("jwk-urls")) {
+                JWKSource<?> jwkSet = new RemoteJWKSet<>(new URL(jwkUrl), jwkRetriever);
+                JWKSelector jwkSelector = new JWKSelector(new JWKMatcher.Builder().keyUse(KeyUse.SIGNATURE).build());
+                List<JWK> jwks = jwkSet.get(jwkSelector, null);
+                for (JWK jwk : jwks) {
+                    SignatureConfiguration signatureConfiguration = parseSignature(jwk);
+                    if (signatureConfiguration != null) signatures.add(signatureConfiguration);
+                }
+                jwkSelector = new JWKSelector(new JWKMatcher.Builder().keyUse(KeyUse.ENCRYPTION).build());
+                jwks = jwkSet.get(jwkSelector, null);
+                for (JWK jwk : jwks) {
+                    EncryptionConfiguration encryptionConfiguration = parseEncryption(jwk);
+                    if (encryptionConfiguration != null) encryptions.add(encryptionConfiguration);
+                }
+            }
+        }
         if (conf.hasPath("signatures")) {
             for (Config signature : conf.getConfigList("signatures")) {
                 SignatureConfiguration signatureConfiguration = parseSignature(signature);
